6716a4a2a01519fb49462c5329da483819597995
Priyanka-Tripathi98 <priyankatripa35@gmail.com>
2026-07-06 17:30:20 +0530

    commit changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import process from "process";
 import { Buffer } from "buffer";
+import emailjs from '@emailjs/browser';
 
-window.Buffer = Buffer; // ✅ fix 3
-window.global = window; // ✅ fix 1
-window.process = { env: {} }; // ✅ fix 2
-
+window.Buffer = Buffer;
+window.global = window;
+window.process = { env: {} };
+emailjs.init("vG69igiSI3mR8BND-");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
